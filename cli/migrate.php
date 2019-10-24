f3bc7ea279408cf7241e77994d912226bde1bcde
Kelson da Costa Medeiros <kelsoncm@gmail.com>
2019-10-24 13:30:41 -0300

    [UPD] cli_interface movido para a locallib.php [UPD] com o aumento da memória pude ler mais linhas, daí forcei o bulkinsertsize para 1000 e assim pude ler de 10k em 10k linhas, o tempo de migrar 13.082.143 linhas diminuiu de 9h42m55s para 1h20m26s , na mesma máquina com os mesmos dados. Causa? A paginação no MySQL vai ficando mais lento à medida em que se encaminha para o final.

--- cli/migrate.php
+++ cli/migrate.php
@@ -15,88 +15,7 @@ require_once(__DIR__.'/../locallib.php');
 
 $tput_cols = intval(exec('tput cols'));
 
-class cli_interface {
-
-    protected $help = null;
-    protected $version = null;
-
-    function cli_progress_bar($progress, $total, $info="", $width=50) {
-        global $tput_cols;
-        $perc = round(($progress * 100) / $total);
-        $bar = round(($width * $perc) / 100);
-        $pb = sprintf("%s%%[%s>%s]", $perc, str_repeat("=", $bar), str_repeat(" ", $width-$bar));
-        $info_p = str_pad($info, $tput_cols-strlen($pb)-4, ' ');
-        return "$pb$info_p\r";
-    }
-    
-    function get_help() {
-        if (empty($this->help)) {
-            return "No help to you.";
-        }
-        $help = $this->help;
-        
-        if ($this->get_option_args() == 0) {
-            return $help;
-        }
-
-        $help .= "\nOptions:\n";
-        $len = 0;
-        foreach ($this->get_option_args() as $option => $configs) {
-            $pading_option = $option . (array_key_exists('value', $configs) ? "={$configs['value']}" : '');
-            $len = strlen($pading_option) > $len ? strlen($pading_option) : $len;
-        }
-        foreach ($this->get_option_args() as $option => $configs) {
-            $pading_option = str_pad($option . (array_key_exists('value', $configs) ? "={$configs['value']}" : ''), $len, " ");
-            $option_help = array_key_exists('help', $configs) ? $configs['help'] : 'No help to you today.';
-            $choices = array_key_exists('choices', $configs) ? 'Choices are: '. implode(', ', $configs['choices']) . '.' : '';
-            $default = array_key_exists('default', $configs) && !empty($configs['default']) ? "Default are {$configs['default']}." : '';
-            $help .= "   --$pading_option $option_help $choices$default\n";
-        }
-        $help .= '';
-        return  $help;
-    }
-
-    function get_version() {
-        return $this->version ? $this->version : 'Moodle version: ' . get_config('', 'version') . "\n";
-    }
-
-    public function get_option_args() {
-        return [
-            "help"=>['label'=>['databasetypehead', 'install'], 'help'=> 'Print this page and exit.'],
-            "version"=>['label'=>['databasetypehead', 'install'], 'help'=> 'Print version number and exit.'],
-        ];
-    }
-
-    function read_options() {
-        $options_to_read = [];
-        foreach ($this->get_option_args() as $option => $configs) {
-            $options_to_read[$option] = array_key_exists('default', $configs) ? $configs['default'] : null;
-        }
-        list($options, $unrecognized) = cli_get_params($options_to_read, []);
-
-        if ($options['help']) {
-            echo $this->get_help();
-            exit(0);
-        }
-
-        if ($options['version']) {
-            echo $this->get_version();
-            exit(0);
-        }
-
-        foreach ($this->get_option_args() as $option => $configs) {
-            if ( !isset($options[$option]) && array_key_exists('value', $configs) && !empty($configs['value'])) {
-                $label = array_key_exists('label', $configs) && !empty($configs['label']) ? get_string($configs['label'][0], $configs['label'][1]) : "Type $option";
-                $choices = array_key_exists('choices', $configs) && !empty($configs['choices']) ? $configs['choices'] : null;
-                $default = array_key_exists('default', $configs) && !empty($configs['default']) ? $configs['default'] : null;
-                $label_to_print = !empty($choices) ? "$label. Choices: " . implode($choices) : $label; 
-                $options[$option] = cli_input($label_to_print, $default, $choices, !empty($choices));
-            }
-        }
-        $this->options=$options;
-    }
-}
-
+ini_set('memory_limit', '1G');
 
 class dbmigrate_migrate extends cli_interface {
     protected $targetdb = null;
@@ -153,7 +72,8 @@ class dbmigrate_migrate extends cli_interface {
         }
         try {
             $this->page_size = !empty($this->options['page_size']) ? intval($this->options['page_size']) : 1000;
-            $dboptions['bulkinsertsize'] = $this->page_size;
+            // $dboptions['bulkinsertsize'] = $this->page_size;
+            $dboptions['bulkinsertsize'] = 1000;
             $this->targetdb->connect($options['dbhost'], $options['dbuser'], $options['dbpass'], $options['dbname'], $options['prefix'], $dboptions);
             $this->target_tables = $this->targetdb->get_tables();
             echo "Connection successful.\n\n";
@@ -338,6 +258,7 @@ class dbmigrate_migrate extends cli_interface {
                 $migrated_in_table_f = number_format($j, 0, "", ".");
 
                 $rs = $DB->get_recordset($table_name, null, 'id', '*', $j+1, $this->page_size);
+                // $rs = $DB->get_recordset($table_name, null, 'id', '*');
                 $this->targetdb->insert_records($table_name, $rs);
                 $rs->close();
             }
--- locallib.php
+++ locallib.php
@@ -48,3 +48,85 @@ function tool_dbmigrate_get_drivers() {
     return ['pgsql/native'=>'PostgreSQL (native/pgsql)'];
     return $drivers;
 }
+
+class cli_interface {
+
+    protected $help = null;
+    protected $version = null;
+
+    function cli_progress_bar($progress, $total, $info="", $width=50) {
+        global $tput_cols;
+        $perc = round(($progress * 100) / $total);
+        $bar = round(($width * $perc) / 100);
+        $pb = sprintf("%s%%[%s>%s]", $perc, str_repeat("=", $bar), str_repeat(" ", $width-$bar));
+        $info_p = str_pad($info, $tput_cols-strlen($pb)-4, ' ');
+        return "$pb$info_p\r";
+    }
+    
+    function get_help() {
+        if (empty($this->help)) {
+            return "No help to you.";
+        }
+        $help = $this->help;
+        
+        if ($this->get_option_args() == 0) {
+            return $help;
+        }
+
+        $help .= "\nOptions:\n";
+        $len = 0;
+        foreach ($this->get_option_args() as $option => $configs) {
+            $pading_option = $option . (array_key_exists('value', $configs) ? "={$configs['value']}" : '');
+            $len = strlen($pading_option) > $len ? strlen($pading_option) : $len;
+        }
+        foreach ($this->get_option_args() as $option => $configs) {
+            $pading_option = str_pad($option . (array_key_exists('value', $configs) ? "={$configs['value']}" : ''), $len, " ");
+            $option_help = array_key_exists('help', $configs) ? $configs['help'] : 'No help to you today.';
+            $choices = array_key_exists('choices', $configs) ? 'Choices are: '. implode(', ', $configs['choices']) . '.' : '';
+            $default = array_key_exists('default', $configs) && !empty($configs['default']) ? "Default are {$configs['default']}." : '';
+            $help .= "   --$pading_option $option_help $choices$default\n";
+        }
+        $help .= '';
+        return  $help;
+    }
+
+    function get_version() {
+        return $this->version ? $this->version : 'Moodle version: ' . get_config('', 'version') . "\n";
+    }
+
+    public function get_option_args() {
+        return [
+            "help"=>['label'=>['databasetypehead', 'install'], 'help'=> 'Print this page and exit.'],
+            "version"=>['label'=>['databasetypehead', 'install'], 'help'=> 'Print version number and exit.'],
+        ];
+    }
+
+    function read_options() {
+        $options_to_read = [];
+        foreach ($this->get_option_args() as $option => $configs) {
+            $options_to_read[$option] = array_key_exists('default', $configs) ? $configs['default'] : null;
+        }
+        list($options, $unrecognized) = cli_get_params($options_to_read, []);
+
+        if ($options['help']) {
+            echo $this->get_help();
+            exit(0);
+        }
+
+        if ($options['version']) {
+            echo $this->get_version();
+            exit(0);
+        }
+
+        foreach ($this->get_option_args() as $option => $configs) {
+            if ( !isset($options[$option]) && array_key_exists('value', $configs) && !empty($configs['value'])) {
+                $label = array_key_exists('label', $configs) && !empty($configs['label']) ? get_string($configs['label'][0], $configs['label'][1]) : "Type $option";
+                $choices = array_key_exists('choices', $configs) && !empty($configs['choices']) ? $configs['choices'] : null;
+                $default = array_key_exists('default', $configs) && !empty($configs['default']) ? $configs['default'] : null;
+                $label_to_print = !empty($choices) ? "$label. Choices: " . implode($choices) : $label; 
+                $options[$option] = cli_input($label_to_print, $default, $choices, !empty($choices));
+            }
+        }
+        $this->options=$options;
+    }
+}
